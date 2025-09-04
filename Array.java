f8d84d62990ca1de95c7ed4e725f571821985b5d
Jay Saraswat <jaysaraswat38@gmail.com>
2025-09-04 17:28:10 +0530

    commit

---
+++ Array.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Array {
+    public static void main(String[] args) {
+        // System.out.println("Enter number");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        // int sum=0;
+        for(int i=0;i>arr[];i++){
+            System.out.println(arr);
+        }
+
+        
+        }
+    }
+
+
--- basiccalculator.java
+++ basiccalculator.java
@@ -1,3 +1,7 @@
 public class basiccalculator {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println(" ");
+    }
     
 }
